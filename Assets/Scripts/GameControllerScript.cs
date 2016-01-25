b2f90ef36197d6886cf0a825679d0c1ff041da1c
Rohit Moni <rohitmdxb@gmail.com>
2016-01-24 17:42:12 -0800

    First Commit

---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+internal struct GameData
+{
+    public int CurrentLevel;
+
+};
+
+
+public class GameControllerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
